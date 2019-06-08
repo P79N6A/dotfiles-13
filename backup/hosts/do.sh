79ecfa6e67e352d72bf28f780e5bc64299fe0e46
iceCory <icecory520@gmail.com>
2019-06-08 14:59:55 +0800

    fix hosts tool

--- backup/hosts/do.sh
+++ backup/hosts/do.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-
-
-function do-hosts (){
+function do-hosts() {
     if [[ $1 == "set" ]]; then
-
-        now=$(date +%Y-%m-%d)
-        sudo cp /etc/hosts /etc/hosts-${now}.back
+        if [[ $(wc -l /etc/hosts | awk '{print $1}') -lt 100 ]]; then
+            echo "back up origin hosts file"
+            sudo mv /etc/hosts.back
+        fi
         sudo bash -c "curl https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts-compact > /etc/hosts"
         sudo systemctl restart NetworkManager
 
     elif [[ $1 == "reset" ]]; then
+        if [[ -f /etc/hosts.back ]]; then
+            echo "restore /etc/hosts.back"
+            sudo mv /etc/hosts.back /etc/hosts
+        else
+            echo "file /etc/hosts.back not exist~"
+        fi
 
     else
         echo "Nothing to be done!"
     fi
-
-    # for win
-    # ipconfig /flushdns
-    # for mac
-    # sudo killall -HUP mDNSResponder
 }
 
 function _do-hosts() {
